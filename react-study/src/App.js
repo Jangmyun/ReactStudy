d0636f8a83d5b2fb7c2fc474ff88020c9d6641cb
Jangmyun <cndlsrb2739@gmail.com>
2024-06-26 03:30:45 +0900

    useEffect 예제

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,10 +1,33 @@
 import Button from "./Button";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const click = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const inputChange = (e) => {
+    setKeyword(e.target.value);
+    console.log(keyword);
+  };
+  console.log("all the time");
+  useEffect(() => {
+    console.log("CALL API");
+  }, []);
+  console.log("Search For", keyword);
+
   return (
     <div>
-      <h1>Welcome</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={inputChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={click}>Click</button>
     </div>
   );
 }
