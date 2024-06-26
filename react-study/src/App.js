44ce37d55e7ee6264b12ee2948fbf0f46c24b977
Jangmyun <cndlsrb2739@gmail.com>
2024-06-26 16:30:30 +0900

    Basic todo List

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,9 +1,41 @@
-import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const changeInput = (e) => {
+    setToDo(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Do ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={changeInput}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
