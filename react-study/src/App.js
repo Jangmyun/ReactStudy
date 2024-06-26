93f581e56bc32052722a4ecbd399067ef2e9b01d
Jangmyun <cndlsrb2739@gmail.com>
2024-06-26 14:48:07 +0900

    init App

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -3,33 +3,7 @@ import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const click = () => {
-    setCounter((prev) => prev + 1);
-  };
-  const inputChange = (e) => {
-    setKeyword(e.target.value);
-    console.log(keyword);
-  };
-  console.log("all the time");
-  useEffect(() => {
-    console.log("CALL API");
-  }, []);
-  console.log("Search For", keyword);
-
-  return (
-    <div>
-      <input
-        value={keyword}
-        onChange={inputChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={click}>Click</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
