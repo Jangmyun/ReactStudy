d85dbdeeeef7932ebe38e8ab87bf2a503d35631b
Jangmyun <cndlsrb2739@gmail.com>
2024-06-26 23:52:03 +0900

    Page routing

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,41 +1,18 @@
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
-import Movie from "./Movie";
+import Movie from "./components/Movie";
+import Home from "./routes/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              mediumCoverImage={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/movie" element={<Detail></Detail>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
