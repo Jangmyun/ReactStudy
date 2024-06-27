7e69578eaa6586e195ebe27821ab41184bf2cb75
Jangmyun <cndlsrb2739@gmail.com>
2024-06-27 10:04:38 +0900

    Link to movieDetail page

--- react-study/src/components/Movie.js
+++ react-study/src/components/Movie.js
@@ -1,8 +1,12 @@
-function Movie({ mediumCoverImage, title, summary, genres }) {
+import { Link } from "react-router-dom";
+
+function Movie({ mediumCoverImage, title, summary, genres, id }) {
   return (
     <div>
       <img src={mediumCoverImage} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((genre) => (
