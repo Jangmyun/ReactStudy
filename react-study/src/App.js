3f407033a8ad833a9a42489f455b58a7a3d39b51
Jangmyun <cndlsrb2739@gmail.com>
2024-06-27 13:40:03 +0900

    Route path 수정

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,6 +1,3 @@
-import styles from "./App.module.css";
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
 import Home from "./routes/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
@@ -10,7 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/movie" element={<Detail></Detail>}></Route>
+        <Route path="/movie/:id" element={<Detail></Detail>}></Route>
       </Routes>
     </Router>
   );
